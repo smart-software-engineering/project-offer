3476544b9cff5ad24fc6396777b887776c7a3fac
Rico Metzger <rico.metzger@gmail.com>
2025-04-08 22:08:56 +0000

    basic main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/smart-software-engineering/project-offer
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":80", nil)
+}
